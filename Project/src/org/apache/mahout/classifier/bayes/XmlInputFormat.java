bf746cfb698948ad4554d1677829963e446d513c
rjodhani <rjodhani1@gmail.com>
2016-03-20 00:04:08 -0400

    Update XmlInputFormat.java

--- Project/src/org/apache/mahout/classifier/bayes/XmlInputFormat.java
+++ Project/src/org/apache/mahout/classifier/bayes/XmlInputFormat.java
@@ -1,9 +1,16 @@
 package org.apache.mahout.classifier.bayes;
 
 
-
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 import java.io.IOException;
-
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
